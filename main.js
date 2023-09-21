dc916745dd42333edea2bab71a19954c1ac5f490
Anna <annamotsikula@gmail.com>
2023-09-21 20:00:57 +0400

    interface vs class

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 // var a = 16;
 // var b = 18;
 // ES6  let & const 
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var x = 20; //explicit
 x = 33;
 var y = 123; // implicit
@@ -9,9 +24,6 @@ var myString = 'TypeScript Introduction';
 var isCompleted = false;
 var nums = [1, 2, 3, 4, 5, 34, 23, 423, 4];
 var nums2 = ['10', '20', '30', '40'];
-var myObj;
-myObj = 13;
-myObj.map(function (i) { return i * 2; });
 var student = {
     name: 'John',
     lastName: 'Doe',
@@ -42,3 +54,38 @@ var Course = /** @class */ (function () {
 }());
 var myCourse = new Course('Typescript lecture for entry level', "Angular");
 console.log(myCourse);
+var Rectangle = /** @class */ (function () {
+    function Rectangle(width, height) {
+        this.w = width;
+        this.h = height;
+    }
+    Rectangle.prototype.area = function () {
+        return this.w * this.h;
+    };
+    Rectangle.prototype.perimeter = function () {
+        return 2 * (this.w + this.h);
+    };
+    return Rectangle;
+}());
+var rec = new Rectangle(4, 5);
+var resultText = 'The surface area of rectangle is ' + rec.area();
+console.log(resultText);
+console.log("The surface area of rectangle is ".concat(rec.area()));
+var Square = /** @class */ (function (_super) {
+    __extends(Square, _super);
+    function Square(side, color) {
+        var _this = _super.call(this, side, side) || this;
+        _this.a = side;
+        _this.color = color;
+        return _this;
+    }
+    return Square;
+}(Rectangle));
+var sq = new Square(9, 'blue');
+console.log("The surface area of square is ".concat(sq.area()));
+console.log("The perimeter of square is ".concat(sq.perimeter()));
+var myCar = {
+    model: 'Mercedes',
+    year: 2021,
+    color: 'black'
+};
--- main.ts
+++ main.ts
@@ -13,9 +13,9 @@ let isCompleted: boolean = false;
 const nums: number[] = [1,2,3,4,5,34,23,423,4]
 const nums2: Array<string> = ['10', '20', '30', '40']
 
-let myObj : any; 
-myObj = 13;
-myObj.map(i=> i*2)
+// let myObj : any; 
+// myObj = 13;
+// myObj.map(i=> i*2)
 // myObj = 'I am an object'
 // myObj = [123,345]
 
@@ -68,5 +68,62 @@ class Course {
 const myCourse = new Course('Typescript lecture for entry level', "Angular")
 
 console.log(myCourse);
+
+interface Shape {
+  color: string
+}
+
+class Rectangle {
+    w: number;
+    h: number;
+
+    constructor(width: number, height: number) {
+        this.w = width;
+        this.h = height
+    }
+
+    area(): number {
+        return this.w * this.h
+    }
+
+    perimeter(): number {
+        return 2 * (this.w + this.h)
+    }
+}
+
+const rec = new Rectangle(4,5);
+let resultText = 'The surface area of rectangle is ' + rec.area();
+console.log(resultText)
+
+console.log(`The surface area of rectangle is ${rec.area()}`)
+
+class Square extends Rectangle implements Shape{
+    color: string;
+    a: number;
+
+    constructor(side: number, color: string) {
+        super(side, side);
+        this.a = side;
+        this.color = color;
+    }
+}
+
+const sq = new Square(9, 'blue');
+
+console.log(`The surface area of square is ${sq.area()}`)
+console.log(`The perimeter of square is ${sq.perimeter()}`)
+
+
+interface Car {
+    model: string,
+    year: number,
+    color: string,
+}
+
+const myCar : Car = {
+    model: 'Mercedes',
+    year: 2021,
+    color: 'black'
+}
 
 
